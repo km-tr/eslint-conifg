59211cd80effb08762c32c9f59813b6303be0813
km-tr <kumagai@bear.tokyo>
2020-07-26 21:29:46 +0900

    add overrides js

--- index.js
+++ index.js
@@ -31,4 +31,14 @@ module.exports = {
       },
     ],
   },
-};
+  overrides: [
+    {
+      files: '*.js',
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+}
